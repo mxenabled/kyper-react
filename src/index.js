465ae68f89a74f628e03603d6cc0b6efabda905e
Cameron Wengert <cameron.wengert@mx.com>
2020-01-14 16:23:49 -0500

    Typo

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import { TokenContext } from './context/TokenContext'
 import { TokenProvider } from './context/TokenProvider'
 
 export {
-  ThemeContext,
-  ThemeProvider,
+  TokenContext,
+  TokenProvider,
 }
